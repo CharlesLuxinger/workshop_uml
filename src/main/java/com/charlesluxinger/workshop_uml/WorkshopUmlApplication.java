772bfd3fc74b9ea851aa1479008fdb03738a7f51
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-03 18:09:50 -0300

    5. Criando operação de instanciação

--- src/main/java/com/charlesluxinger/workshop_uml/WorkshopUmlApplication.java
+++ src/main/java/com/charlesluxinger/workshop_uml/WorkshopUmlApplication.java
@@ -1,13 +1,31 @@
 package com.charlesluxinger.workshop_uml;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WorkshopUmlApplication {
+import com.charlesluxinger.workshop_uml.domain.Categoria;
+import com.charlesluxinger.workshop_uml.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class WorkshopUmlApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopUmlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
