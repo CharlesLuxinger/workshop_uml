90a09411e1e3f39042ec40ea08cec68e1c73768c
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-03 17:09:10 -0300

    1. Criado Projeto

---
+++ src/main/java/com/charlesluxinger/workshop_uml/WorkshopUmlApplication.java
@@ -0,0 +1,13 @@
+package com.charlesluxinger.workshop_uml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopUmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopUmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charlesluxinger/workshop_uml/WorkshopUmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlesluxinger.workshop_uml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopUmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
