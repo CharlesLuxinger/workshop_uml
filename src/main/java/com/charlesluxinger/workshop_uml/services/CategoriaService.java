effc8e80e1a57d0af830706690186750f37e7419
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-04 00:16:39 -0300

    7. Endpoint /categorias/{id}

--- src/main/java/com/charlesluxinger/workshop_uml/services/CategoriaService.java
+++ src/main/java/com/charlesluxinger/workshop_uml/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.charlesluxinger.workshop_uml.domain.Categoria;
 import com.charlesluxinger.workshop_uml.repositories.CategoriaRepository;
+import com.charlesluxinger.workshop_uml.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
