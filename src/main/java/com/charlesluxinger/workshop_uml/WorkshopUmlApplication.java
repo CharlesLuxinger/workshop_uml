37246848eeaee0aab05b7322d3a52577fe0ad7dd
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-04 00:43:01 -0300

    8. Estado e cidade

--- src/main/java/com/charlesluxinger/workshop_uml/WorkshopUmlApplication.java
+++ src/main/java/com/charlesluxinger/workshop_uml/WorkshopUmlApplication.java
@@ -8,19 +8,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.charlesluxinger.workshop_uml.domain.Categoria;
+import com.charlesluxinger.workshop_uml.domain.Cidade;
+import com.charlesluxinger.workshop_uml.domain.Estado;
 import com.charlesluxinger.workshop_uml.domain.Produto;
 import com.charlesluxinger.workshop_uml.repositories.CategoriaRepository;
+import com.charlesluxinger.workshop_uml.repositories.CidadeRepository;
+import com.charlesluxinger.workshop_uml.repositories.EstadoRepository;
 import com.charlesluxinger.workshop_uml.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class WorkshopUmlApplication implements CommandLineRunner{
+public class WorkshopUmlApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopUmlApplication.class, args);
 	}
@@ -29,20 +39,33 @@ public class WorkshopUmlApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.0);
 		Produto p2 = new Produto(null, "Impressora", 800.0);
 		Produto p3 = new Produto(null, "Mouse", 80.0);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
